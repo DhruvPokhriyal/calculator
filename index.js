9c6fa573bf809097717c41fed1e9992a4e98e895
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-23 16:55:48 +0530

    Add the result evaluation functionality to the calculator

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 function sum(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 function operate(num1, num2, operator) {
+    let output;
     switch (operator) {
         case "+":
-            sum(num1, num2);
+            output = sum(num1, num2);
             break;
         case "-":
-            subtract(num1, num2);
+            output = subtract(num1, num2);
             break;
         case "*":
-            multiply(num1, num2);
+            output = multiply(num1, num2);
             break;
         case "/":
-            divide(num1, num2);
+            output = divide(num1, num2);
             break;
     }
+    console.log(output);
 }
 
 function showOperation(e) {
-    resultBox = document.querySelector(".numeric-result");
-    buttonClicked = e.target.textContent;
+    const resultBox = document.querySelector(".numeric-result");
+    const buttonClicked = e.target.textContent;
     resultBox.textContent += buttonClicked;
     // console.log(buttonClicked);
 }
@@ -44,9 +46,27 @@ for (let button of numberButtonList) {
 }
 
 function allClear(e) {
-    resultBox = document.querySelector(".numeric-result");
+    const resultBox = document.querySelector(".numeric-result");
     resultBox.textContent = "";
 }
 
+function resultEvaluation(e) {
+    const resultBox = document.querySelector(".numeric-result");
+    let data = resultBox.textContent;
+    let listedData = [];
+    for (let i = 0; i < data.length; i++) {
+        elem = data[i];
+        if ("+-*/".includes(elem)) {
+            listedData = data.split(elem);
+            listedData.push(elem);
+            break;
+        }
+    }
+    console.log(listedData);
+    operate(...listedData);
+}
+
+const evaluator = document.querySelector(".result-calculation");
+evaluator.addEventListener("click", resultEvaluation);
 const ac = document.querySelector(".clear");
 ac.addEventListener("click", allClear);
