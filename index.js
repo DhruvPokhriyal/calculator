5b896764795c9f58b7701dc718272839fdabfaf1
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-25 18:34:39 +0530

    Add feature for rounding off the division upto 2 decimal

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ let numString = "";
 let OPERATOR_STRING = "+-*/";
 
 function sum(num1, num2) {
-    return Number(num1) + Number(num2);
+    return Math.round(Number(num1) + Number(num2), 2);
 }
 
 function subtract(num1, num2) {
-    return Number(num1) - Number(num2);
+    return Math.round(Number(num1) - Number(num2), 2);
 }
 
 function multiply(num1, num2) {
-    return Number(num1) * Number(num2);
+    return Math.round(Number(num1) * Number(num2), 2);
 }
 
 function divide(num1, num2) {
-    return Number(num1) / Number(num2);
+    return Math.round(Number(num1) / Number(num2), 2);
 }
 
 function operate(num1, operator, num2) {
