071847ce79336ba7f1abc565d7a72a46e2edc3a6
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-09-25 12:29:14 +0530

    Hold the history of calculations in a list
    The above method is implemented so that list can store the history of calculations and multiple calculations can be chained togethor

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let history = [];
+
 function sum(num1, num2) {
     return Number(num1) + Number(num2);
 }
@@ -14,7 +16,7 @@ function divide(num1, num2) {
     return Number(num1) / Number(num2);
 }
 
-function operate(num1, num2, operator) {
+function operate(num1, operator, num2) {
     let output;
     switch (operator) {
         case "+":
@@ -37,6 +39,7 @@ function showOperation(e) {
     const resultBox = document.querySelector(".numeric-result");
     const buttonClicked = e.target.textContent;
     resultBox.textContent += buttonClicked;
+    history.push(buttonClicked);
     // console.log(buttonClicked);
 }
 
@@ -48,23 +51,36 @@ for (let button of numberButtonList) {
 function allClear(e) {
     const resultBox = document.querySelector(".numeric-result");
     resultBox.textContent = "";
+    history = [];
 }
 
 function resultEvaluation(e) {
-    const resultBox = document.querySelector(".numeric-result");
-    let data = resultBox.textContent;
-    let listedData = [];
-    for (let i = 0; i < data.length; i++) {
-        elem = data[i];
-        if ("+-*/".includes(elem)) {
-            listedData = data.split(elem);
-            listedData.push(elem);
-            break;
-        }
+    // // const resultBox = document.querySelector(".numeric-result");
+    // let data = history;
+    // let listedData = [];
+    // for (let i = 0; i < data.length; i++) {
+    //     elem = data[i];
+    //     if ("+-*/".includes(elem)) {
+    //         listedData = data.split(elem);
+    //         listedData.push(elem);
+    //     }
+    // }
+    // // if (listedData.length > 3) {
+    // //     let operatedOutput = operate(...listedData.slice(0, 3));
+    // //     resultBox.textContent = String(operatedOutput) + listedData[3];
+    // //     return;
+    // // }
+    // // console.log(listedData);
+    // let evaluatedResult = operate(...listedData);
+    // resultBox.textContent = evaluatedResult;
+    if (history.length >= 3) {
+        const resultBox = document.querySelector(".numeric-result");
+        let result = operate(history[0], history[1], history[2]);
+        history.splice(0, 3, result);
+        console.log(result);
+        console.log(history);
+        resultBox.textContent = result;
     }
-    // console.log(listedData);
-    let evaluatedResult = operate(...listedData);
-    resultBox.textContent = evaluatedResult;
 }
 
 const evaluator = document.querySelector(".result-calculation");
